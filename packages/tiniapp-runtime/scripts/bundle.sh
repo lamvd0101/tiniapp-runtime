7766a517ca8d9a8a1a57a65f8f28b5afc5c3d146
Liam Vuong <lamvd0101@gmail.com>
2022-09-22 16:13:45 +0700

    Sync

---
+++ packages/tiniapp-runtime/scripts/bundle.sh
@@ -0,0 +1,18 @@
+CLI_PATH=./node_modules/react-native/local-cli/cli.js
+COMPOSE_SOURCE_MAPS_PATH=./node_modules/react-native/scripts/compose-source-maps.js
+
+INTEGRATION_MODULE_IOS=../tiniapp-sdk-ios/Rescources
+IOS_BUNDLE_NAME=tiniapp-runtime.jsbundle
+
+if [ -d "${INTEGRATION_MODULE_IOS}" ]; then
+  rm -rf $INTEGRATION_MODULE_IOS
+fi
+mkdir $INTEGRATION_MODULE_IOS
+
+# iOS
+node $CLI_PATH ram-bundle \
+  --entry-file ./index.js \
+  --platform ios \
+  --dev false \
+  --bundle-output $INTEGRATION_MODULE_IOS/$IOS_BUNDLE_NAME \
+  --assets-dest $INTEGRATION_MODULE_IOS
