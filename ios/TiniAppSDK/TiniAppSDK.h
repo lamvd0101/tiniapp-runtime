284cbeeb5b16b100ad11d0df79947797ffc329d8
Liam Vuong <lamvd0101@gmail.com>
2022-09-23 15:16:30 +0700

    Sync

---
+++ ios/TiniAppSDK/TiniAppSDK.h
@@ -0,0 +1,19 @@
+//
+//  TiniAppSDK.h
+//  TiniAppSDK
+//
+//  Created by LAP02092 on 23/09/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TiniAppSDK.
+FOUNDATION_EXPORT double TiniAppSDKVersionNumber;
+
+//! Project version string for TiniAppSDK.
+FOUNDATION_EXPORT const unsigned char TiniAppSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TiniAppSDK/PublicHeader.h>
+
+
--- scripts/build-fw-ios.sh
+++ scripts/build-fw-ios.sh
@@ -1,15 +1,19 @@
 DIR=$PWD
+
+PROJECT_NAME=TiniAppRuntime
 FRAMEWORK_NAME=TiniAppSDK
+OUT_BUILD_DIR=$DIR/ios/build
+OUT_FW_DIR=$DIR/ios/Frameworks
 
-rm -rf build
-rm -rf Frameworks
+rm -rf $OUT_BUILD_DIR
+rm -rf $OUT_FW_DIR
 
-SIMULATOR_ARCHIVE_PATH=$DIR/build/${FRAMEWORK_NAME}-iphonesimulator.xcarchive
-DEVICE_ARCHIVE_PATH=$DIR/build/${FRAMEWORK_NAME}-iphoneos.xcarchive
+SIMULATOR_ARCHIVE_PATH=$OUT_BUILD_DIR/${FRAMEWORK_NAME}-iphonesimulator.xcarchive
+DEVICE_ARCHIVE_PATH=$OUT_BUILD_DIR/${FRAMEWORK_NAME}-iphoneos.xcarchive
 
 # Simulator xcarchieve
 xcodebuild archive \
-  -workspace ${FRAMEWORK_NAME}.xcworkspace \
+  -workspace ios/${PROJECT_NAME}.xcworkspace \
   -scheme ${FRAMEWORK_NAME} \
   -archivePath ${SIMULATOR_ARCHIVE_PATH} \
   -configuration Release \
@@ -20,7 +24,7 @@ xcodebuild archive \
 
 # Device xcarchieve
 xcodebuild archive \
-  -workspace ${FRAMEWORK_NAME}.xcworkspace \
+  -workspace ios/${PROJECT_NAME}.xcworkspace \
   -scheme ${FRAMEWORK_NAME} \
   -archivePath ${DEVICE_ARCHIVE_PATH} \
   -sdk iphoneos \
@@ -35,5 +39,5 @@ for framework in *; do
   xcodebuild -create-xcframework \
     -framework $SIMULATOR_ARCHIVE_PATH/Products/Library/Frameworks/$frameworkName.framework \
     -framework $DEVICE_ARCHIVE_PATH/Products/Library/Frameworks/$frameworkName.framework \
-    -output $DIR/Frameworks/$frameworkName.xcframework
+    -output $OUT_FW_DIR/$frameworkName.xcframework
 done
