1697c385c2bd1f58e02c1bb69b5e3cce63dc5681
Liam Vuong <lamvd0101@gmail.com>
2022-09-26 15:08:24 +0700

    Add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        'no-shadow': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,23 @@
-module.exports = {
-  presets: [
-    [
-      'module:metro-react-native-babel-preset',
-      {
-        unstable_disableES6Transforms: true,
-      },
+module.exports = api => {
+  const isProd = api.cache(() => process.env.NODE_ENV === 'production');
+  let config = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@app': './src',
+          },
+        },
+      ],
     ],
-  ],
+  };
+  if (isProd) {
+    config = {
+      ...config,
+      plugins: [...config.plugins, ['transform-remove-console']],
+    };
+  }
+  return config;
 };
