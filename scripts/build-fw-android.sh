f2ba32eaa1c322dc5e0e46424dc26b9d2ebc6896
Liam Vuong <lamvd0101@gmail.com>
2022-10-05 09:32:56 +0700

    Sync

--- scripts/build-fw-android.sh
+++ scripts/build-fw-android.sh
@@ -1,4 +1,20 @@
-cd android
-# ./gradlew clean TiniAppSDK:assembleRelease
-./gradlew clean -x app:assembleRelease assembleRelease
+DIR=$PWD
+
+# cd android
+# ./gradlew clean -x app:assembleRelease assembleRelease
+# cd -
+
+BUILD_LIBS=$DIR/android/build/libs
+OUTPUT_LIBS=$DIR/android/SDKOutput/libs
+
+rm -rf $OUTPUT_LIBS
+mkdir $OUTPUT_LIBS
+
+cd $BUILD_LIBS
+for lib in *; do
+  libPath=$BUILD_LIBS/$lib/outputs/aar/$lib-release.aar
+  if [ -e "$libPath" ]; then
+    cp $libPath $OUTPUT_LIBS/$lib-release.aar
+  fi
+done
 cd -
