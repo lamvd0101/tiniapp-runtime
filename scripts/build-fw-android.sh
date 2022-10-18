4775852054dad6d15ea02f8c6a61cdf229baa59d
Liam Vuong <lamvd0101@gmail.com>
2022-10-18 13:18:24 +0700

    Sync

--- scripts/build-fw-android.sh
+++ scripts/build-fw-android.sh
@@ -2,24 +2,25 @@ DIR=$PWD
 RN_AAR=$DIR/node_modules/react-native/android/com/facebook/react/react-native/0.61.5/react-native-0.61.5.aar
 HERMES_AAR=$DIR/node_modules/hermes-engine/android/hermes-release.aar
 
+BUILD_LIBS=$DIR/android/build/libs
+OUTPUT=$DIR/android/SDKOutput/libs
+
+rm -rf $OUTPUT
+mkdir $OUTPUT
+
 cd android
 ./gradlew clean -x app:assembleRelease assembleRelease
+./gradlew createGradleFile
 cd -
-
-BUILD_LIBS=$DIR/android/build/libs
-OUTPUT_LIBS=$DIR/android/SDKOutput/libs
-
-rm -rf $OUTPUT_LIBS
-mkdir $OUTPUT_LIBS
 
 cd $BUILD_LIBS
 find . -name "*-release.aar" | while read f; do
   echo "$f"
-  cp $f $OUTPUT_LIBS
+  cp $f $OUTPUT/libs
 done
 cd -
 
-cp $RN_AAR $OUTPUT_LIBS
-cp $HERMES_AAR $OUTPUT_LIBS
+cp $RN_AAR $OUTPUT/libs
+cp $HERMES_AAR $OUTPUT/libs
 
-rm -rf $OUTPUT_LIBS/react-native-camera-mlkit-release.aar
+rm -rf $OUTPUT/libs/react-native-camera-mlkit-release.aar
